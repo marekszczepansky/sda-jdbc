82f74ef1176746908d2a392ed1e8134d8121af0e
marekszczepansky <marek.szczepansky@gmail.com>
2019-02-01 17:48:52 +0100

    solution 1

--- src/main/java/pl/sda/dao/EmpDAOJdbcImpl.java
+++ src/main/java/pl/sda/dao/EmpDAOJdbcImpl.java
@@ -3,7 +3,7 @@ package pl.sda.dao;
 import pl.sda.domain.Employee;
 
 import java.math.BigDecimal;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 /**
@@ -11,38 +11,127 @@ import java.util.List;
  */
 public class EmpDAOJdbcImpl implements EmpDAO {
 
+    private static final String QUERY_BY_ID = "select * from Emp where empno = ?";
+    private static final String INSERT_STMT = "insert into Emp (empno, ename, job, manager, hiredate, salary, commision, deptno) values(?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String UPDATE_STMT = "update emp set ename = ?, job = ?, manager = ?, hiredate = ?, salary = ?, commision = ?, deptno = ? where empno = ?";
+    private static final String DELETE_STMT = "delete from emp where empno = ?";
+    private static final String QUERY_SALARY_BY_DEPT = "select sum(salary) from emp where deptno = ?";
     private final JdbcConnectionManager jdbcConnectionManager;
 
-    public EmpDAOJdbcImpl(JdbcConnectionManager jdbcConnectionManager){
+    EmpDAOJdbcImpl(JdbcConnectionManager jdbcConnectionManager) {
         this.jdbcConnectionManager = jdbcConnectionManager;
     }
 
     @Override
-    public Employee findById(int id) throws Exception {
+    public Employee findById(int id) throws SQLException {
+        try (Connection conn = jdbcConnectionManager.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(QUERY_BY_ID);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return mapFromResultSet(rs);
+            }
+
+        }
         return null;
     }
 
-    @Override
-    public void create(Employee employee) throws Exception {
-
+    private Employee mapFromResultSet(ResultSet rs) throws SQLException {
+        return new Employee(
+                rs.getInt("empno"),
+                rs.getString("ename"),
+                rs.getString("job"),
+                rs.getInt("manager"),
+                rs.getDate("hiredate"),
+                rs.getBigDecimal("salary"),
+                rs.getBigDecimal("commision"),
+                rs.getInt("deptno")
+        );
     }
 
     @Override
-    public void update(Employee employee) throws Exception {
+    public void create(Employee employee) throws SQLException {
+        try (Connection conn = jdbcConnectionManager.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(INSERT_STMT);
+
+            setCreateStatement(employee, ps);
+
+            int numberOfAffectedRows = ps.executeUpdate();
+
+            System.out.println("EmpDAO.create() number of affected rows: " + numberOfAffectedRows);
+        }
 
     }
 
-    @Override
-    public void delete(int id) throws Exception {
-
+    private void setCreateStatement(Employee employee, PreparedStatement ps) throws SQLException {
+        ps.setInt(1, employee.getEmpno());
+        ps.setString(2, employee.getEname());
+        ps.setString(3, employee.getJob());
+        ps.setInt(4, employee.getManager());
+        ps.setDate(5, new Date(employee.getHiredate().getTime()));
+        ps.setBigDecimal(6, employee.getSalary());
+        ps.setBigDecimal(7, employee.getCommision());
+        ps.setInt(8, employee.getDeptno());
     }
 
     @Override
-    public void create(List<Employee> employees) throws Exception {
+    public void update(Employee employee) throws SQLException {
+        try (Connection conn = jdbcConnectionManager.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(UPDATE_STMT);
+
+            ps.setString(1, employee.getEname());
+            ps.setString(2, employee.getJob());
+            ps.setInt(3, employee.getManager());
+            ps.setDate(4, new Date(employee.getHiredate().getTime()));
+            ps.setBigDecimal(5, employee.getSalary());
+            ps.setBigDecimal(6, employee.getCommision());
+            ps.setInt(7, employee.getDeptno());
+
+            ps.setInt(8, employee.getEmpno());
+
+            int numberOfAffectedRows = ps.executeUpdate();
+
+            System.out.println("EmpDAO.update() number of affected rows: " + numberOfAffectedRows);
+        }
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        try (Connection conn = jdbcConnectionManager.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(DELETE_STMT);
+            ps.setInt(1, id);
+
+            int numberOfAffectedRows = ps.executeUpdate();
+
+            System.out.println("EmpDAO.delete() number of affected rows: " + numberOfAffectedRows);
+        }
+    }
+
+    @Override
+    public void create(List<Employee> employees) throws SQLException {
+        try (Connection conn = jdbcConnectionManager.getConnection()) {
+            conn.setAutoCommit(false);
+            PreparedStatement ps = conn.prepareStatement(INSERT_STMT);
+            for (Employee employee : employees) {
+                setCreateStatement(employee, ps);
+                ps.executeUpdate();
+            }
+            conn.commit();
+        }
     }
 
     @Override
     public BigDecimal getTotalSalaryByDept(int dept) throws Exception {
+        try (Connection conn = jdbcConnectionManager.getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(QUERY_SALARY_BY_DEPT);
+            ps.setInt(1, dept);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getBigDecimal(1);
+            }
+        }
         return null;
     }
 }
