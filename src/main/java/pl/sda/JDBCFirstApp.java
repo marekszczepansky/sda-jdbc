2f690d10f01e69ab6862b82e6fc4f8bc023d0b0e
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-01-26 10:54:15 +0100

    java20poz

--- src/main/java/pl/sda/JDBCFirstApp.java
+++ src/main/java/pl/sda/JDBCFirstApp.java
@@ -1,22 +1,32 @@
 package pl.sda;
 
-import pl.sda.dao.DeptDAOJdbcImpl;
-import pl.sda.dao.EmpDAOJdbcImpl;
-import pl.sda.dao.JdbcConnectionManager;
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JDBCFirstApp {
     public static void main(String[] args) throws SQLException, IOException {
-        JdbcConnectionManager jdbcConnectionManager = new JdbcConnectionManager(DbConfiguration.loadConfiguration());
-        DatabaseUtil.cleanUpDatabase(jdbcConnectionManager);
+//        JdbcConnectionManager jdbcConnectionManager = new JdbcConnectionManager(DbConfiguration.loadConfiguration());
+//        DatabaseUtil.cleanUpDatabase(jdbcConnectionManager);
 
-        EmpDAOJdbcImpl empDAO = new EmpDAOJdbcImpl(jdbcConnectionManager);
-        DeptDAOJdbcImpl deptDAO = new DeptDAOJdbcImpl(jdbcConnectionManager);
+//        EmpDAOJdbcImpl empDAO = new EmpDAOJdbcImpl(jdbcConnectionManager);
+//        DeptDAOJdbcImpl deptDAO = new DeptDAOJdbcImpl(jdbcConnectionManager);
 
+        final DbConfiguration dbConfiguration = DbConfiguration.loadConfiguration();
+        final String jdbcUrl = dbConfiguration.getJdbcUrl();
+        final String username = dbConfiguration.getUsername();
+        final String password = dbConfiguration.getPassword();
 
-
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)){
+            PreparedStatement ps = conn.prepareStatement("select * from car");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                System.out.println(rs.getString("reg_number"));
+            }
+        }
 
 
     }
